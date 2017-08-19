87e72696eddbbe08f8243e4e33f140fbdc97d20d
gohilakshay <leoakshay95@gmail.com>
2017-08-19 13:10:46 +0530

    first commit add all files

---
+++ user.php
@@ -0,0 +1,43 @@
+<?php include 'header.php'; ?>
+<div class="content">
+	            <div class="container-fluid">
+	                <div class="row">
+	                    <div class="col-md-8">
+	                        <div class="card">
+	                            <div class="card-header" data-background-color="purple">
+	                                <h4 class="title">Login</h4>
+									<p class="category">Please Enter Your Username and Password</p>
+	                            </div>
+	                            <div class="card-content">
+	                                <form>
+	                                    <div class="row">
+	                                        <div class="col-md-12">
+												<div class="form-group label-floating">
+													<label class="control-label">UserName </label>
+													<input type="text" class="form-control" >
+												</div>
+	                                        </div>
+	                                        
+	                                    </div>
+
+	                                    
+
+	                                    <div class="row">
+	                                        <div class="col-md-12">
+												<div class="form-group label-floating">
+													<label class="control-label">Password</label>
+													<input type="text" class="form-control" >
+												</div>
+	                                        </div>
+	                                    </div>
+                                        <button type="submit" class="btn btn-primary pull-right">Login</button>
+	                                    <div class="clearfix"></div>
+	                                </form>
+	                            </div>
+	                        </div>
+	                    </div>
+                    </div>
+	            </div>
+	        </div>
+     </div>
+ </div>
