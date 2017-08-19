2b01829fe6fcdda29b4a6dfbbeca75f211369315
gohilakshay <leoakshay95@gmail.com>
2017-08-19 17:55:01 +0530

    login added

--- login.php
+++ login.php
@@ -1,5 +1,28 @@
 <?php include 'header.php'; ?>
 <?php include 'config.php'; ?>
+<?php 
+if(isset($_POST['username']) && isset($_POST['password'])){
+    $username = $_POST['username'];
+    $password = $_POST['password'];    
+    $data = array(
+        'username'=>$username,
+        'password'=>$password
+    );
+    # Create a connection
+    $url = 'http://localhost/yogaProject/login_api.php';
+    $ch = curl_init($url);
+    # Form data string
+    $postString = http_build_query($data, '', '&');
+    # Setting our options
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $postString);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    # Get the response
+    $response = curl_exec($ch);
+    //print_r($response);
+    curl_close($ch);
+}
+?>
 <div class="content">
 	            <div class="container-fluid">
 	                <div class="row">
@@ -10,7 +33,7 @@
 									<p class="category">Please Enter Your Username and Password</p>
 	                            </div>
 	                            <div class="card-content">
-	                                <form action="#" method="post">
+	                                <form action="login.php" method="post">
 	                                    <div class="row">
 	                                        <div class="col-md-12">
 												<div class="form-group label-floating">
