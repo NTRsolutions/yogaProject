c400f0ec423171cf53bb7558fed900d4175d9312
ganeshavhad <gavhad218@gmail.com>
2017-08-22 16:37:49 +0530

    colour

--- add_employee_api.php
+++ add_employee_api.php
@@ -2,7 +2,7 @@
 include 'config.php';
 if(isset($_POST['e_name']) && isset($_POST['e_surname']) && isset($_POST['e_salary'])&& isset($_POST['e_contact'])&& isset($_POST['e_address'])){
     
-    $e_name = $_POST['e_name'];
+     $e_name = $_POST['e_name'];
      $e_surname = $_POST['e_surname'];
      $e_salary = $_POST['e_salary'];
      $e_contact = $_POST['e_contact'];
