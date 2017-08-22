e1a4632574ea4ad49998b061d6b74e62071c2749
gohilakshay <leoakshay95@gmail.com>
2017-08-22 17:05:50 +0530

    few update in client

---
+++ view_client_api.php
@@ -0,0 +1,16 @@
+<?php 
+include 'config.php';
+$sql = "SELECT * FROM client ORDER BY c_ID DESC";
+$result = $conn->query($sql);
+$client = array();
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        array_push($client,array('c_ID'=>$row['c_ID'],'c_name'=>$row['c_name'],'c_surname'=>$row['c_surname'],'address'=>$row['address'],'contact'=>$row['contact'],'fees'=>$row['fees'],'status_payment'=>$row['status_payment']));
+    }
+    $client_view = array('client_view'=>$client);
+    echo json_encode($client_view);
+} else {
+    echo "0 results";
+}
+?>
