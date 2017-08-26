acdcb4479be908faf21a9cc73b7fb0d3f8d7065b
gohilakshay <leoakshay95@gmail.com>
2017-08-26 17:43:06 +0530

    client and employee redirect done

--- edit_client_api.php
+++ edit_client_api.php
@@ -12,42 +12,10 @@ if(isset($_POST['c_name']) && isset($_POST['c_surname']) && isset($_POST['c_fees
      $batch_id = $_POST['batch'];
     $sql = "UPDATE `client` SET `c_name` = '$c_name', `c_surname` = '$c_surname', `address` = '$c_address', `contact` = '$c_contact', `fees` = '$c_fees', `batch_id` = '$batch_id' WHERE `client`.`c_ID` = '$c_id'";
     if ($conn->query($sql) === TRUE) {
-        ?> 
-<div class="alert alert-success" role="alert">
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria- hidden="true">&times;</span></button>
-        <strong>Success!</strong> You have been signed in successfully!
-</div>
-<script>
-    window.setTimeout(function() {
-        $(".alert").fadeTo(500, 0).slideUp(500, function(){
-            $(this).remove(); 
-        });
-    }, 1000)
-</script>
-    <?php
-        //echo "<script>alert('Client created successfully')</script>";
-    } else {
-         
-        ?> 
-<div class="alert alert-danger" role="alert">
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria- hidden="true">&times;</span></button>
-        <strong>Success!</strong> Sorry! unsuccessful entry
-</div>
-<script>
-    window.setTimeout(function() {
-        $(".alert").fadeTo(500, 0).slideUp(500, function(){
-            $(this).remove(); 
-        });
-    }, 1000);
-</script>
-    <?php
-
-        
-        echo "While adding Client <br> Error: " . $sql . "<br>" . $conn->error;
+        header('Location: client.php');
+    }
+    else{
+      echo "Code Not Updated";
     }
 }
-else {
-    echo "<script> alert('no Value Found while adding Client') </script>";
-}
-//header('Location: client.php');
 ?>
--- edit_employee_api.php
+++ edit_employee_api.php
@@ -11,42 +11,10 @@ if(/*isset($_POST['e_ID']) &&*/ isset($_POST['e_name']) && isset($_POST['e_surna
     $sql = "UPDATE `employee` SET `e_name` = '$e_name', `e_surname` = '$e_surname', `address` = '$address',`contact` = '$contact', `salary` = '$salary'  WHERE `employee`.`e_ID` = '$eid'";
     if ($conn->query($sql) === TRUE)
     {
-        ?> 
-<div class="alert alert-success" role="alert">
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria- hidden="true">&times;</span></button>
-        <strong>Success!</strong> You have been signed in successfully!
-</div>
-<script>
-    window.setTimeout(function() {
-        $(".alert").fadeTo(500, 0).slideUp(500, function(){
-            $(this).remove(); 
-        });
-    }, 1000)
-</script>
-    <?php
-        //echo "<script>alert('employee created successfully')</script>";
-    } else {
-         
-        ?> 
-<div class="alert alert-danger" role="alert">
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria- hidden="true">&times;</span></button>
-        <strong>Success!</strong> Sorry! unsuccessful entry
-</div>
-
-<script>
-    window.setTimeout(function() {
-        $(".alert").fadeTo(500, 0).slideUp(500, function(){
-            $(this).remove(); 
-        });
-    }, 1000);
-</script>
-    <?php
-
-        
-       echo "While adding employee <br> Error: " . $sql . "<br>" . $conn->error;
+        header('Location: employee.php');
     }
-}
-else {
-    echo "<script> alert('no Value Found while adding employee') </script>";
+    else {
+        echo "Code Not Updated";
+    }
 }
 ?>
