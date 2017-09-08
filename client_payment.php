e9e339314a1d7f1780b511d737a6b833b6266ed9
gohilakshay <leoakshay95@gmail.com>
2017-09-08 10:35:28 +0530

    client_payment view

--- client_payment.php
+++ client_payment.php
@@ -42,7 +42,7 @@ if(isset($_POST['id'])){
                                             <option value="<?php if(isset($_POST["c_id"])){echo $_POST["c_id"];}else echo "" ?>"> <?php if(isset($_POST["c_id"])){echo $_POST["c_id"];}else echo "Select ID"; ?>
                                             </option>
                                             <?php foreach($client_view as $value){?>
-                                            <option value="<?php echo $value->c_ID; ?>"><?php echo $value->c_ID; ?></option>
+                                            <option value="<?php echo $value->c_ID; ?>"><?php echo $value->c_ID." - - ".$value->c_name; ?></option>
                                             <?php } ?> 
                                         </select>
                                     </div>
